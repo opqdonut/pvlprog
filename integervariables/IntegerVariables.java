54b0b500b67ff4c7a04e8be8c59755753e4df504
Paul Tötterman <paul.totterman@iki.fi>
2010-02-20 17:16:37 +0200

    Example code for the integer variables exercise

---
+++ integervariables/IntegerVariables.java
@@ -0,0 +1,9 @@
+public class IntegerVariables {
+	public static void main(String[] args) {
+		int a = 2, b = 42, c = 27, x, y, z;
+		x = a + b;
+		y = b - c;
+		z = x * y / a;
+		System.out.println("x: "+x+" y: "+y+" z: "+z);
+	}
+}
